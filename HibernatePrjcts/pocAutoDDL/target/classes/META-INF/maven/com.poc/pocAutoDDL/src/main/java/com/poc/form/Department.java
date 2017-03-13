f11c4842247500489ef0c24f926e0243b7ee2fc7
Hemanth Peela <Hemanthpeela@192.168.0.101>
2017-03-13 22:28:43 +0530

    Update on CURD Operations

---
+++ HibernatePrjcts/pocAutoDDL/target/classes/META-INF/maven/com.poc/pocAutoDDL/src/main/java/com/poc/form/Department.java
@@ -0,0 +1,49 @@
+package com.poc.form;
+
+public class Department {
+
+	  private int deptID;
+	  private String deptName;
+	  private String deptHOD;
+	  private String deptEvents;
+	  
+	  public int getDeptID()
+	  {
+	    return this.deptID;
+	  }
+	  
+	  public void setDeptID(int deptID)
+	  {
+	    this.deptID = deptID;
+	  }
+	  
+	  public String getDeptName()
+	  {
+	    return this.deptName;
+	  }
+	  
+	  public void setDeptName(String deptName)
+	  {
+	    this.deptName = deptName;
+	  }
+	  
+	  public String getDeptHOD()
+	  {
+	    return this.deptHOD;
+	  }
+	  
+	  public void setDeptHOD(String deptHOD)
+	  {
+	    this.deptHOD = deptHOD;
+	  }
+	  
+	  public String getDeptEvents()
+	  {
+	    return this.deptEvents;
+	  }
+	  
+	  public void setDeptEvents(String deptEvents)
+	  {
+	    this.deptEvents = deptEvents;
+	  }
+}
