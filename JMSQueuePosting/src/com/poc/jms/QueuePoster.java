36ad1f8ccd4fdca0c51360e9a1d36a5490f5791a
Hemanthpeela <Hemanthpeela@192.168.0.102>
2017-04-13 00:05:06 +0530

    JMS Sending Message to Queue

---
+++ JMSQueuePosting/src/com/poc/jms/QueuePoster.java
@@ -0,0 +1,90 @@
+package com.poc.jms;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Hashtable;
+
+import javax.jms.JMSException;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueSender;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class QueuePoster {
+	
+	public final static String SERVER= "t3://localhost:7001";
+	public final static String JNDI_FACTORY= "weblogic.jndi.WLInitialContextFactory";
+	public final static String JMS_FACTORY= "com.poc.base.cf";
+	public final static String QUEUE= "com.poc.base.dq";
+	
+	private QueueConnectionFactory queueConnectionFactory;
+	private QueueSession queueSession;
+	private QueueConnection queueConnection;
+	private QueueSender queueSender;
+	private Queue queue;
+	private TextMessage message;
+	
+	public void init(Context context, String queueName) throws NamingException, JMSException {
+		queueConnectionFactory = (QueueConnectionFactory) context.lookup(JMS_FACTORY);
+		queue = (Queue) context.lookup(QUEUE);
+		queueConnection = queueConnectionFactory.createQueueConnection();
+		queueSession = queueConnection.createQueueSession(false,Session.AUTO_ACKNOWLEDGE);
+		queueSender = queueSession.createSender(queue);
+		message = queueSession.createTextMessage();
+		queueConnection.start();
+	}
+	
+	public void post(String msg) throws JMSException {
+		message.setText(msg);
+		queueSender.send(message);
+	}
+	
+	public void close() throws JMSException {
+		queueSender.close();
+		queueSession.close();
+		queueConnection.close();
+	}
+	
+	private static void sendToServer(QueuePoster queuePoster) throws IOException, JMSException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		boolean readFlag = true;
+		System.out.println("Enter messages to send to WebLogic server (Enter quit to end):n");
+		while(readFlag) {
+			System.out.println("Enter Messages: ");
+			String msg = bufferedReader.readLine();
+			if(msg.equals("quit")){
+				queuePoster.post(msg);
+				System.exit(0);
+			}
+			queuePoster.post(msg);
+			System.out.println();
+		}
+		bufferedReader.close();
+	}
+	
+	private static InitialContext getInitialContext() throws NamingException {
+		/*Properties p = new Properties();
+		p.put(Context.INITIAL_CONTEXT_FACTORY,"weblogic.jndi.WLInitialContextFactory");
+		p.put(Context.PROVIDER_URL, "t3://localhost:7001");*/
+		
+		Hashtable<String, String> env = new Hashtable<String, String>();
+		env.put(Context.INITIAL_CONTEXT_FACTORY,JNDI_FACTORY);
+		env.put(Context.PROVIDER_URL, SERVER);
+		return new InitialContext(env);
+	}
+	
+	public static void main(String args[]) throws Exception {
+		InitialContext initialContext = getInitialContext();
+		QueuePoster queuePoster = new QueuePoster();
+		queuePoster.init(initialContext, QUEUE);
+		sendToServer(queuePoster);
+		queuePoster.close();
+	}
+}
